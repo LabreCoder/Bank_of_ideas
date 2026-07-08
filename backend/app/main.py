2017abb9bd1cd0a047380a2e60648e87e0d2deda
LabreCoder <jvlabremachado@gmail.com>
2026-07-08 20:23:02 -0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,12 +5,14 @@ from fastapi.middleware.cors import CORSMiddleware
 from routes.ideas import router as ideas_router
 from routes.category import router as category_router
 from routes.owner import router as owner_router
+from routes.planning import router as planning_router
 
 app = FastAPI()
 
 app.include_router(ideas_router)
 app.include_router(category_router)
 app.include_router(owner_router)
+app.include_router(planning_router)
 
 @app.get("/")
 def home():
