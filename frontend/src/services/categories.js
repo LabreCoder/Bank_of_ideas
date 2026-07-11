19e0946e01ad1266f8700d2f6e571a794e35c8dd
LabreCoder <jvlabremachado@gmail.com>
2026-07-10 22:31:24 -0300

    Update categories.js service

--- frontend/src/services/categories.js
+++ frontend/src/services/categories.js
@@ -2,4 +2,7 @@ import { api } from "./api";
 
 export const categoriesApi = {
   list: () => api.get("/category/"),
+  create: (payload) => api.post("/category/", payload),
+  update: (id, payload) => api.put(`/category/${id}`, payload),
+  delete: (id) => api.delete(`/category/${id}`),
 };
