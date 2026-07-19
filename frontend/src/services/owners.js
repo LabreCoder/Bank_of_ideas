6782f005fdb975973d3d8b9c3ab2cc5fe87992f3
LabreCoder <jvlabremachado@gmail.com>
2026-07-19 12:42:35 -0300

    Updating my services

--- frontend/src/services/owners.js
+++ frontend/src/services/owners.js
@@ -2,4 +2,7 @@ import { api } from "./api";
 
 export const ownersApi = {
   list: () => api.get("/owner/owners"),
+  create: (payload) => api.post("/owner/", payload),
+  update: (id, payload) => api.put(`/owner/${id}`, payload),
+  delete: (id) => api.delete(`/owner/${id}`),
 };
