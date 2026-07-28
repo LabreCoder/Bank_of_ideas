79c763acf600d680f3b3ffefd384c2c7b56cd8e2
LabreCoder <jvlabremachado@gmail.com>
2026-07-28 20:13:10 -0300

    Updating main with new route: checklist

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,21 +6,26 @@ from routes.ideas import router as ideas_router
 from routes.category import router as category_router
 from routes.owner import router as owner_router
 from routes.planning import router as planning_router
+from routes.checklist import router as checklist_router
 
 app = FastAPI()
+
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(ideas_router)
 app.include_router(category_router)
 app.include_router(owner_router)
 app.include_router(planning_router)
+app.include_router(checklist_router)
 
 @app.get("/")
 def home():
     return {"message": "API Online"}
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
