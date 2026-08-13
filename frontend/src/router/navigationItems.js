9a50e6d2fd1dae03cef2bae1d3477f628aa85ac7
LabreCoder <jvlabremachado@gmail.com>
2026-08-13 20:58:55 -0300

    Updating my navegation items

---
+++ frontend/src/router/navigationItems.js
@@ -0,0 +1,7 @@
+export const NAV_ITEMS = [
+  { to: "/dashboard", label: "Dashboard" },
+  { to: "/ideas", label: "Ideas" },
+  { to: "/planning", label: "Planning" },
+  { to: "/cycle", label: "Cycles" },
+  { to: "/settings", label: "Settings" },
+];
